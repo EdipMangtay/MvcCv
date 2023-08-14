fad4e23f7ea74f53c3c332fda974890bb3542aaa
aeman <aeman@MANGTAY>
2023-08-14 22:06:19 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLHAKKINDAs.ToList();
+
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimeler = db.TBLDENEYIMLERIMs.ToList();
+
+            return PartialView(deneyimeler);
+        }
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TBLEGIITMLERIMs.ToList();
+
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yeteneklerim = db.TBLYETENEKLERIMs.ToList();
+            return PartialView(yeteneklerim);
+
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobilerim = db.TBLHOBILERIMs.ToList();
+            return PartialView(hobilerim);
+
+        }
+        public PartialViewResult Sertifika()
+        {
+            var sertifikar = db.TBLSERTIFIKALARIMs.ToList();
+            return PartialView(sertifikar);
+
+        }
+        [HttpGet]
+        public PartialViewResult iletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult iletisim(TBLILETISIM t)
+        {
+            t.TARIH = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TBLILETISIMs.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }
+}
---
+++ MvcCv/Controllers/DeneyimController.cs
@@ -0,0 +1,20 @@
+﻿using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class DeneyimController : Controller
+    {
+        DeneyimRepositoery repo = new DeneyimRepositoery();
+        // GET: Deneyim
+        public ActionResult Index()
+        {
+            var degerler = repo.list();
+            return View();
+        }
+    }
+}
